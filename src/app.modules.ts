006b6d8203447b3b63b2c2c912ada31424abc269
amd12 <tyr.artem@gmail.com>
2022-02-14 23:52:48 +0300

    Add post

--- src/app.modules.ts
+++ src/app.modules.ts
@@ -7,6 +7,13 @@ import {Role} from "./roles/roles.model";
 import {UserRoles} from "./roles/user-roles.model";
 import {RolesModule} from "./roles/roles.module";
 import {AuthModule} from "./auth/auth.module";
+import {Post} from "./posts/post.module";
+import {PostsModule} from "./posts/posts.module";
+import {ServeStaticModule} from "@nestjs/serve-static";
+
+import * as path from 'path'
+
+import { join } from 'path';
 
 
 @Module({
@@ -23,12 +30,16 @@ import {AuthModule} from "./auth/auth.module";
             username: process.env["POSTGRES_USER"],
             password: process.env["POSTGRES_PASSWORD"],
             database: process.env["POSTGRES_DB"],
-            models: [User, Role, UserRoles],
+            models: [User, Role, UserRoles, Post],
             autoLoadModels: true
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname,'static'),
         }),
         UsersModule,
         RolesModule,
         AuthModule,
+        PostsModule
     ]
 })
 
