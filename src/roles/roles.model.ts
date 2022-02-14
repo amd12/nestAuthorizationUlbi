63839aec84b67c6813bcac7e82f800b47d3ccb4e
amd12 <tyr.artem@gmail.com>
2022-02-14 13:25:53 +0300

    Initial commit

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,31 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
+
+interface UserCreationAttrs {
+    email:string;
+    password:string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User,UserCreationAttrs>{
+
+    @ApiProperty({example: '1',description:'unique identification'})
+    @Column({type: DataType.INTEGER, unique:true, autoIncrement:true, primaryKey: true})
+    id: number;
+
+    @ApiProperty({example: 'twink@gmail.com',description:'Email user'})
+    @Column({type: DataType.STRING, unique:true, allowNull: false})
+    email: string;
+
+    @ApiProperty({example: 'qwery123',description:'Password user'})
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+    @ApiProperty({example: 'true',description:'Banned user'})
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @ApiProperty({example: 'For spoofing',description:'Reason for blocking'})
+    @Column({type: DataType.STRING})
+    banReason: string;
+}
