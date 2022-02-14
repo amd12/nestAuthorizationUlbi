ed9cf5e0435a95ed0b7935a8280746602879f021
amd12 <tyr.artem@gmail.com>
2022-02-14 16:14:32 +0300

    add feature validator

---
+++ src/pipes/validation.pipes.ts
@@ -0,0 +1,9 @@
+import {ArgumentMetadata, Injectable, PipeTransform} from "@nestjs/common";
+
+@Injectable()
+export class ValidationPipes implements PipeTransform<any>{
+    transform(value: any, metadata: ArgumentMetadata): any {
+        return undefined;
+    }
+
+}
